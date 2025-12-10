567d29645cae616b04ebde86e3628b8e0dd22c78
VITOR GOMES BATISTA FEITOSA <vitor.feitosa@aluno.senac.rio>
2025-12-10 18:37:31 -0300

    Conta Esqueleto

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,18 @@
-"""
-URL configuration for config project.
+# urls.py do seu projeto principal (ex: meu_projeto/urls.py)
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView # Para a página inicial simples
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # 🌟 Inclui as views de autenticação prontas do Django
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    # Adicionando uma URL de login mais amigável
+    path('login/', include('django.contrib.auth.urls')),
+    
+    # Pagina Inicial (Target de LOGIN_REDIRECT_URL)
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
