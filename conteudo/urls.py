1853607e4249332e4cf756b2345e081fe14cf2a9
oSkillo <raphaelvcalabria@gmail.com>
2025-12-10 16:46:50 -0300

    sobre

--- conteudo/urls.py
+++ conteudo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='base'),
-    path('contato/', views.contato_view, name='contato')
+    path('contato/', views.contato_view, name='contato'),
+    path('sobre/', views.sobre_view, name='sobre'),
 ]
