e9e09e3186336a7e28c8a98ca2e4638ea626f00a
Vitor Feitosa <vitor.feitosa.dev@gmail.com>
2025-12-15 16:35:30 -0300

    criacao de usuario

--- conta/urls.py
+++ conta/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     # Mapeia o URL para a view 'perfil_usuario' e nomeia o caminho como 'perfil'
     path('perfil/', views.perfil_usuario, name='perfil'),
+    path('register/', views.register, name='register'),
     
     # Se você quiser adicionar funcionalidade para mudar a senha (opcional)
     # path('mudar_senha/', views.mudar_senha, name='mudar_senha'), 
