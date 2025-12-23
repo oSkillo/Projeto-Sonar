7b2634ad8a0875158e668acfcdc842ff2e8cf883
Raphael Calábria <167239724+oSkillo@users.noreply.github.com>
2025-12-23 05:58:46 -0300

    Projeto Sonar Consertado
    * urls funcionais
    
    * urls funcionais e consertaada
    
    * css menos sidebar
    
    * cssBonitao
    
    * super estilizacao
    
    * barra de pesquisa
    
    * barra de pesquisa definitiva
    
    * tentativa-mobile
    
    * paginaLoginVitor
    
    * paginaLoginVitor
    
    * quase 100%?
    
    * Projeto-Sonar100%
    
    * ProjetoSonarEntrega
    
    * ProjetoSonar23/12

--- conteudo/models.py
+++ conteudo/models.py
@@ -4,15 +4,48 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Perfil(models.Model):
-    # O OneToOneField garante que cada usuário tenha apenas UM perfil
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    usuario = models.OneToOneField( 
+        User,
+        on_delete=models.CASCADE, 
+        related_name='perfil', 
+        verbose_name='Client')
+
+    foto = models.ImageField(
+        upload_to='perfis/',
+        default='perfis/default.png',
+        null=True, 
+        blank=True,
+    )
+    # Troca de foto:
+    def save(self, *args, **kwargs):
+        # Se já existe (tem ID)
+        if self.pk:
+            try:
+                # Pega a foto antiga do banco
+                foto_antiga = Perfil.objects.get(pk=self.pk).foto
+                
+                # Se trocou de foto e não é a default
+                if foto_antiga and foto_antiga != self.foto and 'default.png' not in foto_antiga.name:
+                    # Apaga o arquivo antigo
+                    import os
+                    if os.path.exists(foto_antiga.path):
+                        os.remove(foto_antiga.path)       
+            except:
+                pass # Nada acontece a foto é default
+        
+        # Salva normalmente
+        super().save(*args, **kwargs)
+
+
+# class Perfil(models.Model):
+#     # O OneToOneField garante que cada usuário tenha apenas UM perfil
+#     usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
     
-    # Se o usuário não enviar foto, usaremos uma imagem padrão
-    foto = models.ImageField(upload_to='perfis/', default='perfis/default.png')
+#     # Se o usuário não enviar foto, usaremos uma imagem padrão
+#     foto = models.ImageField(upload_to='perfis/', default='perfis/default.png')
 
-    def __str__(self):
-        return f"Perfil de {self.usuario.username}"
-
+#     def __str__(self):
+#         return f"Perfil de {self.usuario.username}"
 
 class Divergencia(models.Model):
     nome = models.CharField(max_length=100)
